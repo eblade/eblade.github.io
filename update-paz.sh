ba7f254fd4e6a2b58f5a952100e65c48c0b1e699
Johan Egneblad <johan@egneblad.se>
2020-08-29 23:16:41 +0200

    Show bishop signature

--- update-paz.sh
+++ update-paz.sh
@@ -4,4 +4,4 @@ if [ -e paz.gpg ]; then
     rm paz.gpg
 fi
 
-cat ~/.pazrc.remote | gpg2 --output paz.gpg --symmetric --cipher-algo aes256 --passphrase $(python3 ~/git/paz/paz.py -s remote) --batch --pinentry-mode=loopback
+cat ~/.pazrc.remote | gpg2 --output paz.gpg --symmetric --cipher-algo aes256 --passphrase $(python3 ~/git/paz/paz.py -bs remote) --batch --pinentry-mode=loopback
